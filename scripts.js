ce58f8081d861816751a8ef8b502a531956bae32
Kanapong Siriprachai <42939643+Kana-P@users.noreply.github.com>
2019-11-10 01:23:02 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-var btn = $('#button');
-
+var btn = $('#toTop');
+// To top button
 $(window).scroll(function () {
     if ($(window).scrollTop() > 300) {
         btn.addClass('show');
@@ -11,4 +11,23 @@ $(window).scroll(function () {
 btn.on('click', function (e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+// Navbar
+$(document).ready(function () {
+    $(".ham a").click(function () {
+        $(".overlay").fadeToggle(200);
+        $(this).toggleClass('btn-open').toggleClass('btn-close');
+        if (document.body.style.overflowY == "hidden")
+            document.body.style.overflowY = "scroll";
+        else document.body.style.overflowY = "hidden";
+    });
+});
+$('.overlay').on('click', function () {
+    $(".overlay").fadeToggle(200);
+    $(".ham a").toggleClass('btn-open').toggleClass('btn-close');
+    if (document.body.style.overflowY == "scroll")
+        document.body.style.overflowY = "hidden";
+    else document.body.style.overflowY = "scroll";
+    open = false;
 });
